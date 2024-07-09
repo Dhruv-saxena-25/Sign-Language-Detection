fd3e7d7999ada74c3743bf137eb20e376aa6f7d2
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-07-09 16:02:55 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Dhruv',
+    author_email= 'saxena25dhruv@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
