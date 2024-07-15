b58f02e5bcde25c5e82436649dbaea8005ecbd4a
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-07-15 15:00:02 +0530

    final model

---
+++ signLanguage/constant/application.py
@@ -0,0 +1,2 @@
+APP_HOST= "127.0.0.1"                       # For the AWC EC2 "0.0.0.0"  
+APP_PORT = 8080
---
+++ train.py
@@ -0,0 +1,9 @@
+from signLanguage.pipeline.training_pipeline import TrainPipeline
+
+
+
+
+if __name__ == "__main__":
+    obj = TrainPipeline()
+    obj.run_pipeline()
+    
